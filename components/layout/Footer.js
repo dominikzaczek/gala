f21c30d4bc335bf403e4799a48d191bac7e1a086
John <2505311+RatherSlowTurtle@users.noreply.github.com>
2022-04-23 23:45:15 +0100

    updates

---
+++ components/layout/Footer.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+export default function Footer() {
+  return (
+    <div className="container-fluid shadow mt-4" style={styles.container}>
+      <div className="container">
+        <Image
+          src="/gala_logo.png"
+          alt="Gala Logo"
+          width="186px"
+          height="90px"
+        />
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    backgroundColor: "black",
+    minHeight: 100,
+    padding: "1em",
+  },
+};
