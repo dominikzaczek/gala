549c0133fe623e581963ea40a1a72642c74e38d6
dominikzaczek <63166007+dominikzaczek@users.noreply.github.com>
2022-04-27 19:09:09 +0100

    List blocked

--- pages/alltickets.js
+++ pages/alltickets.js
@@ -1,7 +1,8 @@
-export default function AllTickets({content}){
-    console.log("CONTENT", content)
-    return <div className="info-container-no-anim">
-        {content.map((tickets, key) => {
+export default function AllTickets({ content }) {
+  console.log("CONTENT", content);
+  return (
+    <div className="info-container-no-anim">
+      {/* {content.map((tickets, key) => {
             return <div key={key}>
                 <h4>{tickets.attributes.owner_name}</h4>
                 <h5>{tickets.attributes.full_table ? `Full table bought. Table name: ${tickets.attributes.table_name}` : `Number of tickets bought: ${tickets.attributes.list_of_guests.length}`}</h5>
@@ -11,29 +12,31 @@ export default function AllTickets({content}){
                 })}
                 <hr />
             </div>
-        })}
-        
-        
-        </div>
+        })} */}
+
+      <h1>List of guests unavailable</h1>
+    </div>
+  );
 }
 
-export async function getServerSideProps({context}){
-    const fetchit = await fetch(
-        process.env.STRAPI_URL + "/api/tickets?pagination[page]=1&pagination[pageSize]=100",
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-          },
-        }
-      );
+export async function getServerSideProps({ context }) {
+  const fetchit = await fetch(
+    process.env.STRAPI_URL +
+      "/api/tickets?pagination[page]=1&pagination[pageSize]=100",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+      },
+    }
+  );
 
-      if (fetchit.ok) {
-        const content = await fetchit.json();
-        return {
-          props: {
-            content: content.data,
-          },
-        };
-      }
-      throw new Error("Could not fetch frontPage data.");
+  if (fetchit.ok) {
+    const content = await fetchit.json();
+    return {
+      props: {
+        content: content.data,
+      },
+    };
+  }
+  throw new Error("Could not fetch frontPage data.");
 }
